95818f55224e7a06b7ee32a9fade1d98f6486900
skom <ainiwijoyo4@gmail.com>
2024-10-29 13:49:58 +0700

    ddd

--- config/conn.php
+++ config/conn.php
@@ -4,5 +4,17 @@ $username = "root";
 $password = "";
 $database = "db_sisarpras";
 
-$con = mysqli_connect($hostname, $username, $password, $database) or die(mysqli_error($con));
+$con = mysqli_connect($hostname, $username, $password, $database);
+if (!$con) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
+
+// Cek apakah file ini dipanggil secara langsung
+if (basename($_SERVER['PHP_SELF']) == basename(__FILE__)) {
+    echo "Koneksi ke database berhasil!";
+    
+    // Opsional: Tambahkan informasi lebih lanjut
+    echo "<br>Server Info: " . mysqli_get_server_info($con);
+    echo "<br>Host Info: " . mysqli_get_host_info($con);
+}
 ?>
